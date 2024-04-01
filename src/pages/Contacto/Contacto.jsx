ba0b5a9cc45f13f0d58bff8be728a132aed67540
Rumanito12 <ady_9697@yahoo.com>
2024-04-01 12:00:02 +0200

    UPDATE: se mejora el diseño de la pagina de Inicio.

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,7 +1,15 @@
+import Button from 'react-bootstrap/Button';
 export const Contacto = () => {
-    return (
-      <>
-        <h1>Contacto</h1>
-      </>
-    );
-  };
+  return (
+    <>
+      <h1>Contacto</h1>
+
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
+    </>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import "./Home.scss"
 export const Home = () => {
     return (
       <div id="Home_Design">
-          <h1>Home</h1>   
+          <h1>Restaurante La Bunica</h1>  
+          <div className="horizontal-line-home"></div> 
       </div>
     );
   };
