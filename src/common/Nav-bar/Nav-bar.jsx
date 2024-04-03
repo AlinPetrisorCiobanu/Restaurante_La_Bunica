f658b1a8a51d84784a6f8cd41077dddad848ec56
Rumanito12 <ady_9697@yahoo.com>
2024-04-03 09:36:03 +0200

    UPDATE: se aregla pequeño fallo en el navbar.

--- src/common/Nav-bar/Nav-bar.jsx
+++ src/common/Nav-bar/Nav-bar.jsx
@@ -1,8 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { useLocation } from "react-router-dom";
-import { Link } from "react-scroll";
+import { useLocation , Link } from "react-router-dom";
 import "./Nav-bar.scss";
 
 export const Nav_bar = () => {
